8b01ab5e64eb9f6ba4cb5f15d26a4e569cec093d
yujieming <780450345@qq.com>
2019-12-23 16:46:25 +0800

    构建java项目

---
+++ red_book-java/src/main/java/com/tcj/red_book/RedBookApplication.java
@@ -0,0 +1,13 @@
+package com.tcj.red_book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedBookApplication.class, args);
+	}
+
+}
---
+++ red_book-java/src/test/java/com/tcj/red_book/RedBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcj.red_book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
